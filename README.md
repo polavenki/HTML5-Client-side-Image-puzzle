e27d7ce56290e50dc6aadcdf875bb153759b9f32
polavenki <pola.venki@gmail.com>
2012-08-21 23:54:59 +0530

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 HTML5-Client-side-Image-puzzle
 ==============================
+
+Please find the demo @ http://polavenki.appspot.com/customizedslider/index.html
---
+++ js/Model/model.js
@@ -0,0 +1,26 @@
+/**
+	@author: Peda Venkateswarlu Pola
+	Email : pola.venki@gmail.com
+	YIM : pola_venki  Gtalk : pola.venki  Skype : pola.venki
+*/
+;(function(w,s){
+
+	s.m.emptyTile = "tile33";
+	s.m.tileSize = 125;
+	s.m.emptyRef = {"x" : 3 , "y" : 2};
+	s.m.state =	[["tile00","tile01","tile02","tile03"],
+	               ["tile10","tile11","tile12","tile13"],
+	               ["tile20","tile21","tile22","tile23"],
+	               ["tile30","tile31","tile32","tile33"]];
+	
+	s.m.solution = [["tile00","tile01","tile02","tile03"],
+		               ["tile10","tile11","tile12","tile13"],
+		               ["tile20","tile21","tile22","tile23"],
+		               ["tile30","tile31","tile32","tile33"]];
+	
+	s.m.alltiles = ["tile00","tile01","tile02","tile03",
+	                "tile10","tile11","tile12","tile13",
+	                "tile20","tile21","tile22","tile23",
+	                "tile30","tile31","tile32","tile33"];
+	
+})(window,slider);
